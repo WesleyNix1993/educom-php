cf1e11a96a45706bc00a27897308ed2e563ee63a
WesleyNix1993 <wesleynix7@gmail.com>
2023-01-10 17:03:07 +0100

    PHP (#1)

--- php-datatypes.php
+++ php-datatypes.php
@@ -22,3 +22,30 @@ $cars = array("Volvo", "BMW", "Toyota");
 var_dump($cars);
 ?>
 
+<?php
+/* Classes */
+class Car {
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+        $this -> color = $color;
+        $this -> model = $model;
+    }
+    public function message() {
+        return "My car is a " . $this->color . " " . $this -> model . "!";
+    }
+}
+
+$myCar = new Car("Black", "Volvo");
+echo $myCar -> message();
+echo "<br>";
+$myCar = new Car("red", "Toyota");
+echo $myCar -> message();
+?>
+
+<?php
+/* Null Value */
+$x = "Hello world!";
+$x = null;
+var_dump($x);
+?>
